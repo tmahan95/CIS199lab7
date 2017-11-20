10c158ef0fcdf3b8743d0bbc8213d1df5239f965
tmahan95 <tmahan95@users.noreply.github.com>
2017-11-20 15:17:23 -0500

    Lab 7

---
+++ Lab7/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Lab7
+{
+    partial class Lab7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.distTxtBox = new System.Windows.Forms.TextBox();
+            this.subBttn = new System.Windows.Forms.Button();
+            this.distLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // distTxtBox
+            // 
+            this.distTxtBox.Location = new System.Drawing.Point(85, 116);
+            this.distTxtBox.Name = "distTxtBox";
+            this.distTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.distTxtBox.TabIndex = 0;
+            // 
+            // subBttn
+            // 
+            this.subBttn.Location = new System.Drawing.Point(99, 156);
+            this.subBttn.Name = "subBttn";
+            this.subBttn.Size = new System.Drawing.Size(75, 23);
+            this.subBttn.TabIndex = 1;
+            this.subBttn.Text = "Submit";
+            this.subBttn.UseVisualStyleBackColor = true;
+            this.subBttn.Click += new System.EventHandler(this.subBttn_Click);
+            // 
+            // distLbl
+            // 
+            this.distLbl.Location = new System.Drawing.Point(43, 81);
+            this.distLbl.Name = "distLbl";
+            this.distLbl.Size = new System.Drawing.Size(186, 32);
+            this.distLbl.TabIndex = 2;
+            this.distLbl.Text = "Enter the distance you wish to travel (whole miles)";
+            this.distLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Lab7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.distLbl);
+            this.Controls.Add(this.subBttn);
+            this.Controls.Add(this.distTxtBox);
+            this.Name = "Lab7";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox distTxtBox;
+        private System.Windows.Forms.Button subBttn;
+        private System.Windows.Forms.Label distLbl;
+    }
+}
+
---
+++ Lab7/Form1.cs
@@ -0,0 +1,49 @@
+﻿//Timothy Mahan
+//Lab 7
+//Due 3/29/16
+//Course Section 01
+/*This program compares the data input by a user and compares it to a distance array, then the program
+  matches the position in the distance array with a corresponding index position in the price array, this 
+  price is then displayed in a message box, which is to inform the user how much it will cost to travel their
+  input distance.*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7
+{
+    public partial class Lab7 : Form
+    {
+        public Lab7()
+        {
+            InitializeComponent();
+        }
+
+        private void subBttn_Click(object sender, EventArgs e)
+        {
+            ushort [] distance = {0,100,300,500};//array to hold distance values
+            ushort [] price = {25,40,55,70};//array to hold price values
+            ushort miles = 0;//integer variable to hold distance a user will travel
+            ushort index = 0;//integer variable to hold index number of arrays
+
+            if (ushort.TryParse(distTxtBox.Text, out miles))//ensure data entered is valid
+            {
+                for (index = (ushort)(distance.Length-1); distance[index] > miles && index >= 0; index--)//search distance index to match user input
+                {
+                   
+                }
+                MessageBox.Show(price[index].ToString());//show price user will pay
+            }
+            else
+            {
+                MessageBox.Show("Please enter a whole number greater than or equal to 0"); //what displays if data is invalid
+            }
+        }
+    }
+}
